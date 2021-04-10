77a8201d71a38b92398b5c0fe20761b4a14e300a
Lucas Marques <lucas.msilva09@gmail.com>
2021-04-10 17:29:11 -0300

    Create the SignIn page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,31 @@
-import Head from 'next/head'
-
-export default function Home() {
+import { Flex, Button, Stack } from '@chakra-ui/react'
+import { Input } from '../components/Form/Input'
+export default function SignIn() {
   return (
-    <h1>Hello world</h1>
+    <Flex
+      w="100vw"
+      h="100vh"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth="360px"
+        bg="gray.800"
+        padding="8"
+        borderRadius="8px"
+        flexDirection="column"
+        >
+          <Stack spacing="4">
+            <Input name="email" type="email" label="E-mail" />
+            <Input name="password" type="password" label="Senha" />
+          </Stack>
+
+          <Button type="submit" marginTop="6" colorScheme="pink" size="lg">
+            Entrar
+          </Button>
+      </Flex>
+    </Flex>
   )
 }
