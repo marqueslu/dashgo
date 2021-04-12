acde9f90313e182d3b064694023ccde7a77c1e66
Lucas Marques <lucas.silva@dextra-sw.com>
2021-04-12 17:52:50 -0300

    Add profile section to header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Text, Input, Icon } from '@chakra-ui/react'
-import { RiSearchLine } from 'react-icons/ri'
+import { Flex, Text, Input, Icon, HStack, Box, Avatar } from '@chakra-ui/react'
+import { RiNotificationLine, RiSearchLine, RiUserAddLine } from 'react-icons/ri'
 
 export function Header() {
   return (
@@ -46,7 +46,30 @@ export function Header() {
       />
       <Icon as={RiSearchLine} fontSize="20" />
       </Flex>
-    </Flex>
 
+      <Flex align="center" marginLeft="auto"
+      >
+        <HStack
+          spacing="8"
+          marginX="8"
+          paddingRight="8"
+          paddingY="1"
+          color="gray.300"
+          borderRightWidth="1px"
+          borderColor="gray.700"
+        >
+          <Icon as={RiNotificationLine} fontSize="20" />
+          <Icon as={RiUserAddLine} fontSize="20" />
+        </HStack>
+
+        <Flex align="center">
+          <Box marginRight="4" textAlign="right">
+            <Text>Lucas Marques</Text>
+            <Text color="gray.300" fontSize="small">lucas.msilva09@gmail.com</Text>
+          </Box>
+          <Avatar size="md" name="Lucas Marques" src="https://github.com/marqueslu.png" />
+        </Flex>
+      </Flex>
+    </Flex>
   )
 }
