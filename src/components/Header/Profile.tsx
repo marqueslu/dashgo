c48fc79f5f643095fe740b9b63202b4c6d400b85
Lucas Marques <lucas.msilva09@gmail.com>
2021-04-17 13:56:08 -0300

    Add responsivity to header components

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Flex, Text, Box, Avatar } from '@chakra-ui/react'
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData: boolean
+}
+
+export function Profile({ showProfileData }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box marginRight="4" textAlign="right">
-        <Text>Lucas Marques</Text>
-        <Text color="gray.300" fontSize="small">
-          lucas.msilva09@gmail.com
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box marginRight="4" textAlign="right">
+          <Text>Lucas Marques</Text>
+          <Text color="gray.300" fontSize="small">
+            lucas.msilva09@gmail.com
+          </Text>
+        </Box>
+      )}
 
       <Avatar size="md" name="Lucas Marques" src="https://github.com/marqueslu.png" />
     </Flex>
