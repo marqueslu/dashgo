43762d36fe9954d399d41ed1eebcb8efa5ab41a2
Lucas Marques <lucas.silva@dextra-sw.com>
2021-06-30 12:02:28 -0300

    Configure axios

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -21,13 +21,13 @@ import { useQuery } from 'react-query'
 import { Header } from '../../components/Header'
 import { Pagination } from '../../components/Pagination'
 import { Sidebar } from '../../components/Sidebar'
+import { api } from '../../services/api'
 
 export default function UserList() {
-  const { data, isLoading, error } = useQuery(
+  const { data, isLoading, isFetching, error } = useQuery(
     'users',
     async () => {
-      const response = await fetch('http://localhost:3000/api/users')
-      const data = await response.json()
+      const { data } = await api.get('users')
 
       const users = data.users.map((user) => {
         return {
@@ -45,7 +45,7 @@ export default function UserList() {
       return users
     },
     {
-      staleTime: 1000 * 15
+      staleTime: 1000 * 5
     }
   )
 
@@ -71,6 +71,9 @@ export default function UserList() {
           <Flex marginBottom="8" justify="space-between" align="center">
             <Heading size="lg" fontWeight="normal">
               Usuários
+              {!isLoading && isFetching && (
+                <Spinner size="sm" color="gray.500" ml="4" />
+              )}
             </Heading>
             <Link href="/users/create" passHref>
               <Button
