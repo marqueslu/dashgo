d55878f4b8c479c403d44fdb1609a88d167fa318
Lucas Marques <lucas.silva@dextra-sw.com>
2021-06-30 18:56:42 -0300

    Add dynamic pagination functionality

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,7 +1,44 @@
 import { Stack, Button, Box } from '@chakra-ui/react'
 import { PaginationItem } from './PaginationItem'
 
-export function Pagination() {
+interface PaginationProps {
+  totalCountOfRegisters: number
+  registersPerPage?: number
+  currentPage?: number
+  onPageChange?: (page: number) => void
+}
+
+const siblingsCount = 1
+
+function generatePagesArray(from: number, to: number) {
+  return [...new Array(to - from)]
+    .map((_, index) => {
+      return from + index + 1
+    })
+    .filter((page) => page > 0)
+}
+
+export function Pagination({
+  totalCountOfRegisters,
+  registersPerPage = 10,
+  currentPage = 1,
+  onPageChange
+}: PaginationProps) {
+  const lastPage = Math.floor(totalCountOfRegisters / registersPerPage)
+
+  const previousPages =
+    currentPage > 1
+      ? generatePagesArray(currentPage - 1 - siblingsCount, currentPage - 1)
+      : []
+
+  const nextPages =
+    currentPage < lastPage
+      ? generatePagesArray(
+          currentPage,
+          Math.min(currentPage + siblingsCount, lastPage)
+        )
+      : []
+
   return (
     <Stack
       direction={['column', 'row']}
@@ -15,12 +52,50 @@ export function Pagination() {
       </Box>
 
       <Stack direction="row" spacing="2">
-        <PaginationItem number={1} isCurrent />
-        <PaginationItem number={2} />
-        <PaginationItem number={3} />
-        <PaginationItem number={4} />
-        <PaginationItem number={5} />
-        <PaginationItem number={6} />
+        {currentPage > 2 && (
+          <>
+            <PaginationItem number={1} onPageChange={onPageChange} />
+            {currentPage > 3 && (
+              <text color="gray.300" width="8">
+                ...
+              </text>
+            )}
+          </>
+        )}
+        {previousPages.length > 0 &&
+          previousPages.map((page) => (
+            <PaginationItem
+              key={page}
+              number={page}
+              onPageChange={onPageChange}
+            />
+          ))}
+
+        <PaginationItem
+          number={currentPage}
+          isCurrent
+          onPageChange={onPageChange}
+        />
+
+        {nextPages.length > 0 &&
+          nextPages.map((page) => (
+            <PaginationItem
+              key={page}
+              number={page}
+              onPageChange={onPageChange}
+            />
+          ))}
+
+        {currentPage + siblingsCount < lastPage && (
+          <>
+            {currentPage + 2 < lastPage && (
+              <text color="gray.300" width="8">
+                ...
+              </text>
+            )}
+            <PaginationItem number={lastPage} onPageChange={onPageChange} />
+          </>
+        )}
       </Stack>
     </Stack>
   )
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -22,9 +22,11 @@ import { Header } from '../../components/Header'
 import { Pagination } from '../../components/Pagination'
 import { Sidebar } from '../../components/Sidebar'
 import { useUsers } from '../../services/hooks/useUsers'
+import { useState } from 'react'
 
 export default function UserList() {
-  const { data, isLoading, isFetching, error } = useUsers()
+  const [page, setPage] = useState(1)
+  const { data, isLoading, isFetching, error } = useUsers(page)
 
   const isWideVersion = useBreakpointValue({
     base: false,
@@ -87,7 +89,7 @@ export default function UserList() {
                   </Tr>
                 </Thead>
                 <Tbody>
-                  {data.map((user) => {
+                  {data.users.map((user) => {
                     return (
                       <Tr key={user.id}>
                         <Td paddingX={['4', '4', '6']}>
@@ -118,7 +120,11 @@ export default function UserList() {
                   })}
                 </Tbody>
               </Table>
-              <Pagination />
+              <Pagination
+                totalCountOfRegisters={data.totalCount}
+                currentPage={page}
+                onPageChange={setPage}
+              />
             </>
           )}
         </Box>
